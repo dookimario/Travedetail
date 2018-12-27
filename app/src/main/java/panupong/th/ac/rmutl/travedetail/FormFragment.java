9d0d0ee88abd10f5d6dbb71625fcf7bccfd035c0
Ducthkun <dookimario@gmail.com>
2018-12-27 09:49:01 +0700

    Spinner Success

--- app/src/main/java/panupong/th/ac/rmutl/travedetail/FormFragment.java
+++ app/src/main/java/panupong/th/ac/rmutl/travedetail/FormFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -22,6 +24,7 @@ import java.util.ArrayList;
 public class FormFragment extends Fragment {
     //    Explicit
     private String nameString, surnameString, genderString, ageString, travelString;
+    private boolean ageABoolean = true; // true Not Choose Age
 
 
     public FormFragment() {
@@ -40,8 +43,28 @@ public class FormFragment extends Fragment {
     }   // Main Method
 
     private void ageController() {
-        String[] strings = new String[]{"0 - 10","11 - 20","21 - 30","31 - 40","41 - 50","Over 51"};
+        final String[] strings = new String[]{"Please Choose Age","0 - 10","11 - 20","21 - 30","31 - 40","41 - 50","Over 51"};
         Spinner spinner = getView().findViewById(R.id.spnAge);
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_1,strings);
+        spinner.setAdapter(stringArrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ageString = strings[position];
+                if (!(position == 0)) {
+                    ageABoolean = false;
+                } else {
+                    ageABoolean = true;
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
     }
@@ -89,6 +112,8 @@ public class FormFragment extends Fragment {
                     myAlert.normalDialog("Have space", "Please Fill Every Blank");
                 } else if (!(maleRadioButton.isChecked() ||femaleRadioButton.isChecked())) {
                     myAlert.normalDialog("Non Gender","Please Choose Male or Female");
+                }else if (ageABoolean){
+                    myAlert.normalDialog("Non Choose Age","Please Choose Age");
                 }
 
             } //onClick
